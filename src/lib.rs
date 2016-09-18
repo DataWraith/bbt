8a19ffab169a2abc1aec5e8ff3ca27f6cc94ac69
Johannes Holzfuß <johannes@holzfuss.name>
2016-09-18 11:55:30 +0200

    Add inline documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,134 @@
+//! BBT is an implementation of a skill-rating system similar to Elo, Glicko or
+//! TrueSkill. It follows `Algorithm 1` from the paper [A Bayesian Approximation
+//! Method for Online Ranking][ABAMOR].
+//!
+//! [ABAMOR]: http://jmlr.csail.mit.edu/papers/volume12/weng11a/weng11a.pdf
+//!
+//! ## Usage
+//!
+//! As a first step, you need to instantiate a Rater:
+//!
+//! ```rust
+//! let rater = bbt::Rater::new(25.0/6.0);
+//! ```
+//!
+//! The new() function takes one parameter, β. This parameter describes how much
+//! randomness (variance in outcomes) your game has. For example, a game like
+//! Hearthstone is much more luck-based than chess and should have a higher
+//! variance; you may need to experiment to see which value has the highest
+//! predictive power.
+//!
+//! ### Two-player games (e.g. Chess)
+//!
+//! BBT has a convenience function for two-player games that returns the new
+//! ratings for the two players after a game. In the example, p1 wins against
+//! p2:
+//!
+//! ```rust
+//! let p1 = bbt::Rating::default();
+//! let p2 = bbt::Rating::default();
+//! let (new_p1, new_p2) = rater.duel(p1, p2, bbt::Outcome::Win);
+//! ```
+//!
+//! The `bbt::Outcome` enum can take on the values `Win`, `Loss` and `Draw`.
+//!
+//! ### Multiplayer games
+//!
+//! Games with more than two players will have to use the general
+//! `update_ratings` method. It takes a vector of teams and a vector of ranks,
+//! with each team being a vector of player ratings. If no error occurs, the
+//! method returns a vector of the same form as the input with updated ratings.
+//!
+//! #### Example 1: Racing Game
+//!
+//! In a racing game without teams, each player is represented as a "team" of
+//! one, and since there are usually no ties in a racing game, the list of ranks
+//! contains no duplicates:
+//!
+//! ```rust
+//! let p1 = bbt::Rating::default();
+//! // ...
+//! let p6 = bbt::Rating::default();
+//!
+//! let new_ratings = rater.update_ratings(vec![vec![p1], vec![p2], vec![p3],
+//!                                             vec![p4], vec![p5], vec![p6]],
+//!                                        vec![1, 2, 3, 4, 5, 6]).unwrap();
+//! ```
+//!
+//! In the example, the first player places first, the second player second, and
+//! so on.
+//!
+//! #### Example 2: Tied Teams
+//!
+//! Let's say you have a hypothetical game with four teams and two players per
+//! team.
+//!
+//! | Team 1 | Team 2  | Team 3 | Team 4 |
+//! | ------ | ------- | ------ | ------ |
+//! | Alice  | Charlie | Eve    | Gabe   |
+//! | Bob    | Dave    | Fred   | Henry  |
+//!
+//! If Team 1 wins, and Team 2 and 3 draw for second place and Team 4 loses, you
+//! can call the `update_ratings` function as follows:
+//!
+//! ```rust
+//! rater = bbt::Rater::default();
+//!
+//! alice = bbt::Rating::default();
+//! bob   = bbt::Rating::default();
+//! // ...
+//! henry = bbt::Rating::default();
+//!
+//! let new_ratings = rater::update_ratings(vec![vec![alice, bob],
+//!                                              vec![charlie, dave],
+//!                                              vec![eve, fred],
+//!                                              vec![gabe, henry]],
+//!                                         vec![1, 2, 2, 4]).unwrap();
+//! ```
+//!
+//! The second vector assigns a rank to the teams given in the first vector.
+//! Team 1 placed first, teams 2 and 3 tie for second place and team 4 comes in
+//! fourth.
+//!
+//! ## Rating scale
+//!
+//! The default rating scale follows TrueSkill's convention of ranks from 0 to 50.
+//! You should be able to use a different scale by specifying the middle of that
+//! scale in `Rating::new()`. For example, to use a more traditional scale of 0 to
+//! 3000, you can initialize ratings with `Rating::new(1500.0, 1500.0/3.0)`. You'll
+//! also need to adjust the β-value of the Rater instance accordingly:
+//! `Rater::new(1500.0/6.0)`.
+
 use std::fmt;
 
+/// Rater is used to calculate rating updates given the β-parameter.
 pub struct Rater {
     beta_sq: f64
 }
 
 impl Rater {
-    fn new(beta: f64) -> Rater {
+    /// This method instantiates a new rater with the given β-parameter.
+    pub fn new(beta: f64) -> Rater {
         Rater { beta_sq: beta * beta }
     }
 }
 
 impl Default for Rater {
+    /// This method instantiates a new rater the default β-parameter of 25.0/6.0
+    /// used in the paper.
     fn default() -> Rater {
         Rater::new(25.0/6.0)
     }
 }
 
 impl Rater {
-    fn update_ratings(&self,
+    /// This method takes a vector of teams, with each team being a vector of
+    /// player ratings, and a vector ranks of the same size that specifies the
+    /// order in which the team finished a game. It returns either
+    /// `Err(error_message)` if the input is incorrect or
+    /// `Ok(Vec<Vec<Rating>>)`. The returned vector is an updated version of
+    /// the `teams` vector that was passed into the function.
+    pub fn update_ratings(&self,
                       teams: Vec<Vec<Rating>>,
                       ranks: Vec<usize>) -> Result<Vec<Vec<Rating>>, &'static str> {
 
@@ -111,12 +222,16 @@ impl Rater {
         Ok(result)
     }
 
-    fn duel(&self, p1: Rating, p2: Rating, outcome: Outcome) -> (Rating, Rating) {
+    /// This method calculates the new ratings for two players after a
+    /// head-to-head duel. The outcome is from the first player `p1`'s
+    /// perspective, i.e. `Win` if the first player won, `Loss` if the second
+    /// player won and `Draw` if neither player won.
+    pub fn duel(&self, p1: Rating, p2: Rating, outcome: Outcome) -> (Rating, Rating) {
         let teams = vec![vec![p1], vec![p2]];
         let ranks = match outcome {
             Outcome::Win => vec![1, 2],
             Outcome::Loss => vec![2, 1],
-            Outcome::Tie => vec![1, 1],
+            Outcome::Draw => vec![1, 1],
         };
 
         let result = self.update_ratings(teams, ranks).unwrap();
@@ -125,12 +240,19 @@ impl Rater {
     }
 }
 
-enum Outcome {
+/// Outcome represents the outcome of a head-to-head duel between two players.
+pub enum Outcome {
+    /// The first player won the game
     Win,
+
+    /// The first player lost the game
     Loss,
-    Tie
+
+    /// Neither player won
+    Draw
 }
 
+/// Rating represents the skill of a player.
 #[derive(PartialEq, Clone)]
 pub struct Rating {
     mu: f64,
@@ -139,6 +261,8 @@ pub struct Rating {
 }
 
 impl Default for Rating {
+
+    /// Instantiates a Rating with the default values of mu=25.0 and sigma=25.0/3.0
     fn default() -> Rating {
         Rating{ mu: 25.0, sigma: 25.0/3.0, sigma_sq: f64::powf(25.0/3.0, 2.0) }
     }
@@ -172,11 +296,12 @@ impl Rating {
         Rating { mu: mu, sigma: sigma, sigma_sq: sigma.powf(2.0) }
     }
 
-    fn mu(&self)    -> f64 { self.mu }
-    fn sigma(&self) -> f64 { self.sigma }
-}
+    /// Returns the estimated skill of the player.
+    pub fn mu(&self)    -> f64 { self.mu }
 
-// TODO: Serialization/Deserialization for Rating
+    /// Returns the variance on the estimate of the player's skill.
+    pub fn sigma(&self) -> f64 { self.sigma }
+}
 
 #[cfg(test)]
 mod test {
@@ -208,7 +333,7 @@ mod test {
         let p2 = ::Rating::default();
 
         let rater = ::Rater::default();
-        let (new_p1, new_p2) = rater.duel(p1, p2, ::Outcome::Tie);
+        let (new_p1, new_p2) = rater.duel(p1, p2, ::Outcome::Draw);
 
         assert!(new_p1.mu == 25.0);
         assert!(new_p2.mu == 25.0);
